a6f04ee157a26301fe04ebf6aadaa6d2c8ebe759
codeDiM0N <karpenko.19dima95@yandex.ru>
2022-12-21 19:30:21 +0300

    add Example 003 sum

---
+++ examples/Examples003-Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberA = 31;
+int numberB = 5;
+int result = numberA + numberB; // Добавлена переменная RESULT
+Console.WriteLine(result);
+
+/* Еще один вариант решения
+int numberA = 3;
+int numberB = 5;
+Console.WriteLine(numberA + numberB);
+*/
